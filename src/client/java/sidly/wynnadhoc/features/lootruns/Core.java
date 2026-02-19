a4821af54c2c717f0838405101a42fdd1e425c62
SidOfThe7Cs <Etphonehome7272@gmail.com>
2026-02-19 08:50:05 -0600

    why did that not remove itself

--- src/client/java/sidly/wynnadhoc/features/lootruns/Core.java
+++ src/client/java/sidly/wynnadhoc/features/lootruns/Core.java
@@ -19,7 +19,6 @@ import sidly.wynnadhoc.config.ConfigManager;
 import sidly.wynnadhoc.config.LootrunData;
 import sidly.wynnadhoc.event.*;
 import sidly.wynnadhoc.features.lootruns.enums.*;
-import sidly.wynnadhoc.lootruns.enums.*;
 import sidly.wynnadhoc.utils.ChatMessageUtils;
 import sidly.wynnadhoc.utils.DebugWindow;
 
