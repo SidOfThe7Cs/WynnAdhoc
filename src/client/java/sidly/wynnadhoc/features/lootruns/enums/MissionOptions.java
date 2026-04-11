1faf02a78f16d07fff19b5b6ab93efadcd3282aa
SidOfThe7Cs <Etphonehome7272@gmail.com>
2026-04-11 11:37:01 -0500

    update lr

--- src/client/java/sidly/wynnadhoc/features/lootruns/enums/MissionOptions.java
+++ src/client/java/sidly/wynnadhoc/features/lootruns/enums/MissionOptions.java
@@ -2,41 +2,28 @@ package sidly.wynnadhoc.features.lootruns.enums;
 
 // TODO fruma
 public enum MissionOptions {
-    // should work
     CleansingGreed("Cleansing Greed", "After opening a Flying Chest, remove one of your Curses."),
-    //
     HighRoller("High Roller", "Gain +10 Pulls and +1 End Reward Reroll"),
-    // N/A
     Materialism("Materialism", "All Challenges now additionally have the Yellow Beacon effect."),
-    Hoarder("Hoarder", "After every time you are offered 30 items from Chests, get a random Boon."),
-    JestersTrick("Jester's Trick", "After being offered 20 Items from Flying Chests, randomly get +3 Pulls, +1 Boon, +1 Curse, or +30s."),
-    // TODO add support
+    Hoarder("Hoarder", "After opening 7 Chests, choose 1 Boon out of 3."),
+    JestersTrick("Jester's Trick", "After being offered 30 Items from Flying Chests, randomly get +3 Pulls, +1 Boon, +1 Curse, or +30s."),
     InterestScheme("Interest Scheme", "Gaining 2 Pulls adds an additional Flying Chest to completing your next Yellow Beacon. (Max 12)"),
-    ThrillSeeker("Thrill Seeker", "Red Beacons will reward 1 Boon out of 2."),
-    // N/A
+    ThrillSeeker("Thrill Seeker", "Red Beacons will reward 1 Boon out of 25."),
     OrphionsGrace("Orphion's Grace", "Boons are now 50% more effective."),
-    // descriptoin different in game?
-    Gourmand("Gourmand", "When you get a Boon, get +1 Beacon Reroll."),
-    //
+    OpalOffering("Opal Offering", "After getting a Curse, consume 1 Boon to gain +2 Pulls (+1 per 50% Potency)"),
+    Gourmand("Gourmand", "Gain +1 Beacon Choice when you use a Beacon Reroll (Max 6). Resets when you begin a challenge. "),
     Porphyrophobia("Porphyrophobia", "Getting offered a Purple Beacon gives +1 Curse. Purple Beacons now give twice as many Pulls."),
-    // should work
-    CleansingRitual("Cleansing Ritual", "After finishing a Challenge, consume 1 Curse to gain +1 Challenge."),
-    Equilibrium("Equilibrium", "After getting 2 Curses, gain a random Boon."),
+    SacrificialRitual("Sacrificial Ritual", "After finishing a Challenge, consume 1 Pull to gain +3 Challenge."),
+    RadiantHunter("Radiant Hunter", " \tDefeating a Radiant Challenge mob grants +1 Pulls (5 max per challenge)."),
+    Equilibrium("Equilibrium", "Gaining a Curse will boost the Potency of your next boon by +50%. (Max +300%) "),
     InnerPeace("Inner Peace", "Curses are now half as effective."),
-    // TODO add support
-    Optimism("Optimism", "Gain +1 Pull for every beacon you reroll."),
-    //
-    BackupBeat("Backup Beat", "Every time you add +360s to your timer, gain +1 Beacon Reroll."),
-    // works
+    Optimism("Optimism", "When a beacon is rerolled, it will not be re-offered unless you run out of options."),
+    BackupBeat("Backup Beat", "Every time you add +300s to your timer, gain +1 Beacon Reroll."),
+    Requiem("Requiem", "For the next 15 minutes, Enemy stat increases are voided."),
     Stasis("Stasis", "While picking a Beacon, your timer does not decrease. (Max 5m)"),
-    // pretty sure work
-    Chronokinesis("Chronokinesis", "Chests now give +1 Pull, but consume 10s (+5s per chest) from your Timer. Completing a challenge reduces this penalty by -15s."),
-    // pretty sure work
-    GamblingBeast("Gambling Beast", "After finishing a Challenge, consume 300s from your timer and gain +1 End Reward Reroll."),
-    // works
+    Chronokinesis("Chronokinesis", "(Non-flying) chests now give +1 Pull, but consume 10s (+5s per chest) from your Timer. Completing a challenge reduces this penalty by -15s."),
     Redemption("Redemption", "Gain +1 End Reward Sacrifice."),
-    // untested should work
-    CompleteChaos("Complete Chaos", "After finishing a Challenge, get an 80% chance to gain an additional random Beacon reward.");
+    CompleteChaos("Complete Chaos", "After finishing a Challenge, get an additional random Beacon reward. ");
 
     private final String displayName;
     private final String description;
