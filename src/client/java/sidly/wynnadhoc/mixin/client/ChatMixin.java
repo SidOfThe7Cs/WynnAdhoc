2b7b048408677919bf68110f6d49b571dbc277a2
SidOfThe7Cs <Etphonehome7272@gmail.com>
2026-08-24 20:17:20 -0500

    fic hover events

--- src/client/java/sidly/wynnadhoc/mixin/client/ChatMixin.java
+++ src/client/java/sidly/wynnadhoc/mixin/client/ChatMixin.java
@@ -1,22 +1,26 @@
 package sidly.wynnadhoc.mixin.client;
 
+import com.wynntils.core.text.StyledText;
 import com.wynntils.mc.event.SystemMessageEvent;
-import net.minecraft.text.*;
+import net.minecraft.text.ClickEvent;
 import net.minecraft.text.ClickEvent.*;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import sidly.wynnadhoc.config.ConfigManager;
+import sidly.wynnadhoc.WynnAdhocClient;
 import sidly.wynnadhoc.event.ChatMessageEvent;
-import sidly.wynnadhoc.mixin.client.accessors.StyledTextAccessor;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Mixin(value = com.wynntils.handlers.chat.ChatHandler.class, remap = false)
 public class ChatMixin {
-    @Inject(method = "onSystemChatReceived", at = @At("HEAD"))
+    @Inject(method = "onSystemChatReceived", at = @At("HEAD" ))
     private void onSystemChatReceived(SystemMessageEvent.ChatReceivedEvent event, CallbackInfo ci) {
         String plain = event.getMessage().getString();
         ChatMessageEvent chatMessageEvent = new ChatMessageEvent(plain, event.getStyledText());
@@ -25,27 +29,24 @@ public class ChatMixin {
             event.setCanceled(true);
         }
 
-        StyledTextAccessor accessor = ((StyledTextAccessor) (Object) event.getStyledText());
-        if (accessor == null) return;
-        List<ClickEvent> clickEvents = accessor.getClickEvents();
-        boolean noHoverEvents = accessor.getHoverEvents().isEmpty();
+        MutableText finalText = Text.literal("" );
 
-        StringBuilder sb = new StringBuilder();
-        for (ClickEvent clickEvent : clickEvents) {
-            String clickEventString = clickEventToString(clickEvent);
-            if (ConfigManager.INSTANCE.config.debug.showCmdOnChatHover && noHoverEvents) {
-                if (!sb.isEmpty()) sb.append("\n");
-                sb.append(clickEventString);
-            }
-        }
-        HoverEvent hoverEvent = new HoverEvent.ShowText(Text.literal(sb.toString()));
+        StyledText[] textParts = event.getStyledText().getPartsAsTextArray();
+        Arrays.stream(textParts).forEach(part -> {
+            List<Text> text = part.getComponent().getSiblings();
+            text.forEach(t -> {
+                ClickEvent clickEvent = t.getStyle().getClickEvent();
+                WynnAdhocClient.LOGGER.temp("clickEvent: " + part.getComponent());
+                if (clickEvent != null) {
+                    String clickEventString = clickEventToString(clickEvent);
+                    HoverEvent hoverEvent = new HoverEvent.ShowText(Text.literal(clickEventString));
+                    WynnAdhocClient.LOGGER.temp("found hover event: " + hoverEvent.getAction().asString());
+                    finalText.append(t.copy().setStyle(t.getStyle().withHoverEvent(hoverEvent)));
+                } else finalText.append(t);
+            });
 
-        // copy to avoid infinite recursion
-        if (noHoverEvents && hoverEvent != null) {
-            Style style = event.getMessage().getStyle();
-            Style newStyle = style.withHoverEvent(hoverEvent);
-            ((MutableText) event.getMessage()).setStyle(newStyle);
-        }
+        });
+        event.setMessage(finalText);
     }
 
     @Unique
@@ -94,9 +95,12 @@ public class ChatMixin {
                 case CUSTOM -> {
                     // For CUSTOM, this is plugin/mod specific
                     Custom custom = (Custom) clickEvent;
-                    yield custom.id() + " " + custom.payload();
+                    yield custom.id().getPath() + " " + custom.payload();
                 }
             });
+            if (action.equals(Action.CUSTOM)) {
+                return whatDO;
+            }
             return action.asString() + " " + whatDO;
         }
         return "";
